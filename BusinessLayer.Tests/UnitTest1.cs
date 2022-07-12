600dd11a23295deb04f4c7957d61b536ea140247
ArdaOdabasi <61662021+ArdaOdabasi@users.noreply.github.com>
2022-07-12 22:16:05 +0300

    Successfully Completed
    The project was completed successfully.

---
+++ BusinessLayer.Tests/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace BusinessLayer.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public TestContext TestContext { get; set; }
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML", "Users.xml", "User", DataAccessMethod.Sequential)]
+        [TestMethod]
+        public void DataTest()
+        {
+            var Manager = new UserManager();
+
+            var Ad = TestContext.DataRow["ad"].ToString();
+
+            var Mail = TestContext.DataRow["mail"].ToString();
+
+            var Telefon = TestContext.DataRow["telefon"].ToString();
+
+            var Sonuc = Manager.AddUser(Ad, Mail, Telefon);
+
+            Assert.IsTrue(Sonuc);
+        }
+    }
+}
---
+++ BusinessLayer/UserManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class UserManager
+    {
+        public bool AddUser(string ad, string mail, string telefon)
+        {
+            if (ad.Length < 4)
+                return false;
+
+            if (!Regex.IsMatch(telefon, "[0-9]"))
+                return false;
+
+            if (!mail.Contains("@"))
+                return false;
+
+            return true;
+        }
+    }
+}
